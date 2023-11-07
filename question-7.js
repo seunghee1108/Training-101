bcb8e774ba5d2cdf4e79c8712d90ffb54d224b42
승희 <bangseunghee1108@gmail.com>
2023-11-08 00:52:48 +0900

    문제 7번 완료

---
+++ question-7.js
@@ -0,0 +1,11 @@
+// 문제 7번 : JavaScript에서 null과 undefined의 차이점을 설명하세요.
+
+const a = null;
+console.log(a); // null
+
+
+let b;
+console.log(b); // undefined
+
+// null : 원시 데이터 유형으로 값이 없음을 나타내는 데 사용됨
+// undefined : 변수가 선언되었지만 값을 할당하지 않은 경우에 발생함
