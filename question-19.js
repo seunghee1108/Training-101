dba4422dedfb5cf654fe19c708e264d347b8798f
승희 <bangseunghee1108@gmail.com>
2023-11-12 20:18:25 +0900

    Q19 - 산술 연산

---
+++ question-19.js
@@ -0,0 +1,16 @@
+// 문제 19번 : +, -, *, / 연산자를 사용해 간단 산술 연산을 하는 코드를 작성하세요.
+
+const a = 5;
+const b = 10;
+
+const add = a + b;
+console.log(add);
+
+const minus = a - b;
+console.log(minus);
+
+const mul = a * b ;
+console.log(mul);
+
+const div = a / b ;
+console.log(div);
