608b7833d218640d8b0e44e93f9106b03d839eb3
승희 <bangseunghee1108@gmail.com>
2023-11-19 18:16:18 +0900

    새로운 속성 추가

---
+++ question-33.js
@@ -0,0 +1,11 @@
+// 문제 33번 : 객체에 새로운 속성을 동적응로 추가하는 코드를 작성하세요.
+
+const academy = {
+  "address" : "대덕대로",
+  "number" :  182
+}
+
+academy.name = "그린컴퓨터아트학원";
+
+console.log(academy);
+// { address: '대덕대로', number: 182, name: '그린컴퓨터아트학원' }
