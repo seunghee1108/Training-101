3843a416fb87f0d5485eac40c2527e211ccfb562
승희 <bangseunghee1108@gmail.com>
2023-11-19 20:13:41 +0900

    Q14 - JavaScript-datatype

--- question-14.js
+++ question-14.js
@@ -1,31 +1,26 @@
 // 문제 14번 : 자바스크립트에서 사용하는 데이터 타입들을 나열 하세요.
 
 // * 문자열 String
-
 const a = "Hello";
 console.log(a)  // hello
 
 
 // * Number
-
 const b = 7;
 console.log(b); // 7
 
 
 // * Boolean 
-
 const age = 28;
 const haha = age >= 28;
 console.log(haha);  // true
 
 
 // * Object 
-
 const obj = { name : 'Seung', age : 28 }
 console.log(obj); // { name: 'Seung', age: 28 }
 
 
 // * Array
-
 const num = [ 1, 2, 3, 4, 5];
 console.log(num); // [ 1, 2, 3, 4, 5 ]
