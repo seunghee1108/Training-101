3b016a9b5bf446e430da27b13c9cbbd51508023d
승희 <bangseunghee1108@gmail.com>
2023-11-19 18:19:48 +0900

    Q33 - 객체에 새로운 속성 추가

---
+++ question-34.js
@@ -0,0 +1,11 @@
+// 문제 34번 : 객체의 속성을 삭제하는 코드를 작성하세요.
+
+const academy = {
+  "address" : "대덕대로",
+  "number" :  182,
+  "name" : "그린컴퓨터아트학원"
+}
+
+delete academy.name;
+console.log(academy);
+// { address: '대덕대로', number: 182 }
