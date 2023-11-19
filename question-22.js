b4dcf7b54b76e8c47e2f42325e88c7e4edec60fb
승희 <bangseunghee1108@gmail.com>
2023-11-19 16:56:33 +0900

    Q22 - 복합 조건식

---
+++ question-22.js
@@ -0,0 +1,18 @@
+// 문제 22번 : x가 y와 같은지, 그리고 z가 0이 아닌지 확인하는 복합 조건식을 작성해보세요.
+
+const x = 5;
+const y = 10;
+const z = 0;
+
+if(x === y) {
+  console.log("x와 y는 같습니다.");
+} 
+if(z === 0) {
+  console.log("z는 0입니다.");
+} 
+if(x !== y) {
+  console.log("x는 y와 같지 않습니다.");
+} 
+if(z !== 0) {
+  console.log("z는 0이 아닙니다.");
+}
