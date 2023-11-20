208d886674527607ae33d088b1051b30438e780d
승희 <bangseunghee1108@gmail.com>
2023-11-20 20:28:34 +0900

    Q68 - promise 선언

--- question-68.js
+++ question-68.js
@@ -12,12 +12,3 @@ const newPromise = new Promise((resolve, reject) => {
     }
   }, 1000);
 });
-
-// Promise 사용
-newPromise
-  .then((result) => {
-    console.log("성공:", result);
-  })
-  .catch((error) => {
-    console.error("에러:", error);
-  });
