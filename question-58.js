9cb58923f3041a86102e93138fefb2ea9b6ba206
승희 <bangseunghee1108@gmail.com>
2023-11-20 19:32:14 +0900

    Q58 - 모듈에서 여러 개의 항목을 내보내기

---
+++ question-58.js
@@ -0,0 +1,16 @@
+// 문제 58번 : 모듈에서 여러 개의 항목을 내보내는 코드를 작성하세요.
+
+// 두 수를 더하는 함수
+function add(a, b) {
+  return a + b;
+}
+
+// 두 수를 곱하는 함수
+function multiply(a, b) {
+  return a * b;
+}
+
+// 함수들을 외부로 내보내기
+export { add, multiply };
+
+
