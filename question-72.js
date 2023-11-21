18291bf435ebb8cd13eb1462c2e3df4b2c63e3a5
승희 <bangseunghee1108@gmail.com>
2023-11-21 14:58:59 +0900

    async / await 와 promise 의 차이점

---
+++ question-72.js
@@ -0,0 +1,8 @@
+// 문제 72번 :  async / await 와 promise 의 차이점을 설명하세요.
+
+// async/await 
+//   : async 함수 안에서 await 키워드를 사용하여 비동기 작업의 완료를 기다리고, try-catch 구문을 사용하여 예외 처리를 수행한다.
+//   :  코드가 동기적으로 보이기 때문에 가독성이 향상된다.
+
+// promise 
+//   : then, catch, finally와 같은 메서드를 사용하여 비동기 코드의 성공, 실패, 완료를 처리한다.
