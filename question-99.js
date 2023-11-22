306831b7dbd18ec70f8d12ecb3d599df212e9ec0
승희 <bangseunghee1108@gmail.com>
2023-11-22 19:07:59 +0900

    Q99 - 배열과 객체의 공통점

---
+++ question-99.js
@@ -0,0 +1,17 @@
+// 문제 99번 : JavaScript에서 배열과 객체의 공통점을 설명하세요.
+
+// 데이터를 저장하는 데 사용된다. (배열:인덱스, 객체:키-값)
+// 추가적인 메소드와 속성을 포함할 수 있다.
+// 모두 반복 가능한(iterable) 객체이다.
+
+
+// 배열
+const fruits = ['apple', 'orange', 'banana'];
+console.log(fruits[0]); // 'apple'
+
+// 객체
+const person = {
+  name: 'Bang',
+  age: 28,
+};
+console.log(person.name); // 'Bang'
