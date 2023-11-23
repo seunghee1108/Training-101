47270767fd8608900616940cfd9e3b952b62de2b
승희 <bangseunghee1108@gmail.com>
2023-11-23 21:20:13 +0900

    Q77 - DOM 요소 선택

---
+++ question-77.js
@@ -0,0 +1,13 @@
+// 문제 77번 : DOM 요소를 선택하는 코드를 작성해보세요. 
+
+// document.getElementById 사용
+const myElement = document.getElementById("myElement");
+
+// document.querySelector 사용 (class)
+const exampleElement = document.querySelector(".example");
+
+// document.querySelector 사용 (id)
+const myElementTwo = document.querySelector("#myElement");
+
+// document.querySelectorAll 사용
+const elementsBySelectorAll = document.querySelectorAll(".mySelector");
