47528b9e042f0a68f2eb8cea9395abbb30908702
승희 <bangseunghee1108@gmail.com>
2023-11-23 21:20:45 +0900

    Q78 - DOM 요소의 텍스트 변경

---
+++ question-78.js
@@ -0,0 +1,7 @@
+// 문제 78번 : 선택한 DOM 요소의 텍스트를 변경하는 코드를 작성해보세요. 
+
+    // ID가 'myDiv'인 요소 선택
+    const myDivElement = document.querySelector('#myDiv');
+
+    // 선택한 요소의 텍스트 변경
+    myDivElement.textContent = 'New Text Content';
